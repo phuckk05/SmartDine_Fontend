21817db45ef8bf7edc87481aa3a36b1c36ab6071
phuckk05 <phuckk2101@gmail.com>
2025-09-18 21:47:04 +0700

    update user

--- src/main/java/com/smartdine/backend/UserController.java
+++ src/main/java/com/smartdine/backend/UserController.java
@@ -13,24 +13,42 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    
 
+    //Lấy all user
     @GetMapping
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
-
+    
+    //Tạo user
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
-
+    //lấy user theo id 
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id) {
         return userService.getUserById(id);
     }
-
+    //Xoa User
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
+    //Cap nhat user
+    @PutMapping("/{id}")
+     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+    Optional<User> existingUser = userRepository.findById(id);
+    if (existingUser.isPresent()) {
+        User user = existingUser.get();
+        user.setName(updatedUser.getName());
+        user.setEmail(updatedUser.getEmail());
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+     }
+    
 }
