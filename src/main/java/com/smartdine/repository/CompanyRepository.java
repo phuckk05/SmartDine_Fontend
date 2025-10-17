02d07368b25bdf7a1c7a3f576fa805715c266bb2
phuckk05 <phuckk2101@gmail.com>
2025-10-17 16:31:24 +0700

    Sửa lại tên hàm

--- src/main/java/com/smartdine/repository/CompanyRepository.java
+++ src/main/java/com/smartdine/repository/CompanyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.smartdine.models.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-
+    boolean existsByCompanyCode(String comapanyCode);
 }
