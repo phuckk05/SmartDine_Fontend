a8c2de0f5a2e585701c9e2540609d5804c7be91b
phuckk05 <phuckk2101@gmail.com>
2025-10-19 09:23:08 +0700

    Sửa lại find company

--- src/main/java/com/smartdine/controllers/BranchController.java
+++ src/main/java/com/smartdine/controllers/BranchController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class BranchController {
 
     // Thêm mới branch
     @PostMapping
-    public ResponseEntity<?> createBranch(Branch branch) {
+    public ResponseEntity<?> createBranch(@RequestBody Branch branch) {
         try {
             final Branch createBranch = branchServices.create(branch);
             return ResponseEntity.ok(createBranch);
