59689a335056d0c154158a9ff0e38f3d17b6ec8e
phuckk05 <phuckk2101@gmail.com>
2025-10-22 10:49:04 +0700

    fix loi revervation

--- src/main/java/com/smartdine/models/Reservation.java
+++ src/main/java/com/smartdine/models/Reservation.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -41,7 +40,7 @@ public class Reservation {
     @Column(name = "status_id")
     private Integer statusId;
 
-    @Lob
+    @Column(name = "note", columnDefinition = "TEXT")
     private String note;
 
     @Column(name = "created_at")
