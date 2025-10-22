592f103d8875192a2aad87a75126e385e6eefbc3
phuckk05 <phuckk2101@gmail.com>
2025-10-23 00:21:42 +0700

    Sửa lỗi find - 4

--- src/main/java/com/smartdine/services/OrderServices.java
+++ src/main/java/com/smartdine/services/OrderServices.java
@@ -33,7 +33,7 @@ public class OrderServices {
         LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         LocalDateTime startOfDay = now.withHour(0).withMinute(0).withSecond(0).withNano(0);
         LocalDateTime endOfDay = now.withHour(23).withMinute(59).withSecond(59).withNano(999999999);
-        
+
         return orderRepository.findDistinctTableIdByStatusIdAndCreatedAtBetween(2, startOfDay, endOfDay);
     }
 
@@ -42,7 +42,7 @@ public class OrderServices {
         LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         LocalDateTime startOfDay = now.withHour(0).withMinute(0).withSecond(0).withNano(0);
         LocalDateTime endOfDay = now.withHour(23).withMinute(59).withSecond(59).withNano(999999999);
-        
+
         return orderRepository.findByTableIdAndCreatedAtBetween(tableId, startOfDay, endOfDay);
     }
 
