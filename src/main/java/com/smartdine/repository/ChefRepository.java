7b97bd9e8e5c3369e513da25eabf9094b5a86868
phuckk05 <phuckk2101@gmail.com>
2025-10-28 15:46:44 +0700

    Remove Chef controller/service/repository - model không tồn tại

--- src/main/java/com/smartdine/repository/ChefRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smartdine.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.smartdine.models.Chef;
-
-public interface ChefRepository extends JpaRepository<Chef, Integer> { }
